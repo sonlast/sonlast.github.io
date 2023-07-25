f01e5fcbb4642dca8c7d976486dd086796de89a1
Ijerson Lastimosa Ilalto <ilalto.ijerson02@gmail.com>
2023-07-25 13:39:41 +0800

    Update 7/25

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-var myElement = document.getElementById("tapThis");
 
-myElement.addEventListener("click", function() {
-  if (myElement.style.backgroundColor === "white") {
-    myElement.style.backgroundColor = "black";
-  } else {
-    myElement.style.backgroundColor = "white";
-  }
-});
